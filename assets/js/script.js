e8dcd023b315b8782aa976e801a125795107c39e
Anna Greaves <gilhespy.anna@gmail.com>
2022-05-02 14:26:52 +0200

    Update home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,18 +12,3 @@ let carousel = new bootstrap.Carousel(homeCarousel, {
     interval: 15000,
     wrap: false
 })
-
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('contact_service', 'contact_form', this)
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-    });
-}
